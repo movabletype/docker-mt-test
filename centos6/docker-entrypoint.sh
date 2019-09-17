dcffbfee7ac5cf043420f364c0bfe0d311c492a7
Kenichi Ishigaki <ishigaki@cpan.org>
2019-09-17 11:25:41 +0900

    Install mysql/mariadb earlier, and delay configuration

--- centos6/docker-entrypoint.sh
+++ centos6/docker-entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 service mysqld start
 service memcached start
 
+mysql -e "create database mt_test character set utf8;"
+mysql -e "grant all privileges on mt_test.* to mt@localhost;"
+
 if [ -f t/cpanfile ]; then
     cpanm --installdeps ./t
 fi
--- centos7/docker-entrypoint.sh
+++ centos7/docker-entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
 
-service mysqld start
-service memcached start
+mysql_install_db --user=mysql --skip-name-resolve >/dev/null
+
+bash -c "cd /usr; mysqld_safe --user=mysql --datadir=/var/lib/mysql &"
+sleep 1
+until mysqladmin ping -h localhost --silent; do
+    echo 'waiting for mysqld to be connectable...'
+    sleep 1
+done
+
+mysql -e "create database mt_test character set utf8;"
+mysql -e "create user mt@localhost"
+mysql -e "grant all privileges on mt_test.* to mt@localhost;"
+
+memcached -d -u root
 
 if [ -f t/cpanfile ]; then
     cpanm --installdeps ./t
