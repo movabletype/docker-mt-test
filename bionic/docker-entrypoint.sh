0f4b643cae2b31402816f790079093060c0bf646
Kenichi Ishigaki <ishigaki@cpan.org>
2020-02-10 08:34:21 +0900

    Update trusty and bionic

--- bionic/docker-entrypoint.sh
+++ bionic/docker-entrypoint.sh
@@ -5,6 +5,10 @@ find /var/lib/mysql -type f | xargs touch
 service mysql start
 service memcached start
 
+mysql -e "create database mt_test character set utf8;"
+mysql -e "create user mt@localhost;"
+mysql -e "grant all privileges on mt_test.* to mt@localhost;"
+
 if [ -f t/cpanfile ]; then
     cpm install -g --test --cpanfile=t/cpanfile
 fi
--- trusty/docker-entrypoint.sh
+++ trusty/docker-entrypoint.sh
@@ -5,6 +5,9 @@ find /var/lib/mysql -type f | xargs touch
 service mysql start
 service memcached start
 
+mysql -e "create database mt_test character set utf8;"
+mysql -e "grant all privileges on mt_test.* to mt@localhost;"
+
 if [ -f t/cpanfile ]; then
     cpm install -g --test --cpanfile=t/cpanfile
 fi
