20e5c9be0eb1d63b5cb28d320e0c5094eb0c79cf
Kenichi Ishigaki <ishigaki@cpan.org>
2021-08-05 18:07:04 +0900

    Add postfix

--- bin/update_dockerfile.pl
+++ bin/update_dockerfile.pl
@@ -12,7 +12,7 @@ my %Conf = (
         apt => {
             base => [qw(
                 ca-certificates netbase git make gcc curl ssh locales perl
-                unzip bzip2 procps ssl-cert
+                unzip bzip2 procps ssl-cert postfix
             )],
             images => [qw(
                 perlmagick libgraphics-magick-perl netpbm
@@ -37,7 +37,7 @@ my %Conf = (
         yum => {
             base => [qw(
                 git make gcc curl perl perl-core
-                zip unzip bzip2 which procps
+                zip unzip bzip2 which procps postfix
             )],
             images => [qw(
                 ImageMagick-perl perl-GD GraphicsMagick-perl netpbm-progs
