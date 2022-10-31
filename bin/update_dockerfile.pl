f31535c9f9685bed5940a83032f15fc1af53dbc5
Kenichi Ishigaki <ishigaki@cpan.org>
2022-10-31 14:40:04 +0900

    Add bookworm

--- bin/update_dockerfile.pl
+++ bin/update_dockerfile.pl
@@ -86,6 +86,25 @@ my %Conf = (
         },
         phpunit => 9,
     },
+    bookworm => {
+        from => 'debian:bookworm-slim',
+        base => 'debian',
+        apt  => {
+            _replace => {
+                'mysql-server'       => 'mariadb-server',
+                'mysql-client'       => 'mariadb-client',
+                'libmysqlclient-dev' => '',
+                'phpunit'            => '',
+            },
+            db => [qw( libdbd-mysql-perl )],
+            php => [qw( php-mbstring php-xml )],
+        },
+        cpan => {
+            # https://github.com/tokuhirom/HTML-TreeBuilder-LibXML/pull/17
+            no_test => [qw( HTML::TreeBuilder::LibXML )],
+        },
+        phpunit => 9,
+    },
     bullseye => {
         from => 'debian:bullseye-slim',
         base => 'debian',
@@ -1072,7 +1091,7 @@ find /var/lib/mysql -type f | xargs touch
 % } elsif ($type =~ /^(?:buster|jessie)$/) {
 chown -R mysql:mysql /var/lib/mysql
 % }
-% if ($type =~ /sid|bullseye/) {
+% if ($type =~ /sid|bookworm|bullseye/) {
 service mariadb start
 % } else {
 service mysql start
