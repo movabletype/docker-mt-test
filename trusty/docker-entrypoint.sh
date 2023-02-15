6596d51ffea1f77f5bc0c55ec5e54f748bae426d
Taku Amano <taku@taaas.jp>
2023-02-16 08:50:05 +0900

    Run `update_dockerfile.pl`.

--- trusty/docker-entrypoint.sh
+++ trusty/docker-entrypoint.sh
@@ -9,7 +9,7 @@ mysql -e "create database mt_test character set utf8;"
 mysql -e "grant all privileges on mt_test.* to mt@localhost;"
 
 if [ -f t/cpanfile ]; then
-    cpm install -g --cpanfile=t/cpanfile
+    cpm install --show-build-log-on-failure -g --cpanfile=t/cpanfile
 fi
 
 exec "$@"
