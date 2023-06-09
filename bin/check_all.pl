6b853871e2394ac26b463cd07851cedaa200ef66
Kenichi Ishigaki <ishigaki@cpan.org>
2023-06-09 14:27:14 +0900

    Improve checker

--- bin/check_all.pl
+++ bin/check_all.pl
@@ -7,23 +7,42 @@ use Getopt::Long;
 use Test::More;
 use Mojo::File qw/path/;
 use LWP::UserAgent;
+use TAP::Parser;
+use Term::ANSIColor qw/colorstrip colored/;
 
 my @targets = @ARGV ? @ARGV : glob "*";
 
 LWP::UserAgent->new->mirror("https://raw.githubusercontent.com/movabletype/movabletype/develop/t/cpanfile", "t/cpanfile");
 
+my %summary;
 for my $name (@targets) {
     my $dockerfile = path("$name/Dockerfile");
     next unless -f $dockerfile;
     next if $dockerfile->slurp =~ /EXPOSE/;
     diag "testing $name";
-    ok eval { !system("docker run -it --rm -v\$PWD:/mt -w /mt movabletype/test:$name bash -c 'TEST_IMAGE=$name prove -lv bin/checker.t' 2>&1 | tee log/check_$name.log"); };
-    my $log = path("log/check_$name.log")->slurp;
-    if ($log =~ /Result: FAIL/) {
+    my $res = eval { !system("docker run -it --rm -v\$PWD:/mt -w /mt movabletype/test:$name bash -c 'TEST_IMAGE=$name prove -lv bin/checker.t' 2>&1 | tee log/check_$name.log"); };
+    my ($has_ok, $has_fail, $has_todo) = (0, 0, 0);
+    if ($res) {
+        my $log = path("log/check_$name.log")->slurp;
+        $log = colorstrip($log);
+        my $parser = TAP::Parser->new({source => $log});
+        while (my $result = $parser->next) {
+            next unless $result->is_test;
+            if ($result->is_ok) {
+                $has_ok++;
+                $has_todo++ if $result->raw =~ /# TODO/;
+            } else {
+                next if $result->is_unplanned;
+                $has_fail++;
+            }
+        }
+    }
+    ok $has_ok, $name;
+    if ($has_fail or !$has_ok) {
         rename "log/check_$name.log" => "log/check_error_$name.log";
         unlink "log/check_warn_$name.log";
     } else {
-        if ($log =~ /Failed \(TODO\)/) {
+        if ($has_todo) {
             rename "log/check_$name.log" => "log/check_warn_$name.log";
             unlink "log/check_error_$name.log";
         } else {
@@ -31,5 +50,14 @@ for my $name (@targets) {
             unlink "log/check_warn_$name.log";
         }
     }
+    $summary{$name} = [$has_ok, $has_fail, $has_todo];
 }
 done_testing;
+
+diag "summary";
+for my $name (sort keys %summary) {
+    my ($ok, $fail, $todo) = @{$summary{$name}};
+    my $message = "$name ok: $ok fail: $fail todo: $todo";
+    $message = colored(['red'], $message) if $fail || !$ok;
+    diag $message;
+}
--- cpanfile
+++ cpanfile
@@ -2,6 +2,8 @@ requires 'Data::Section::Simple';
 requires 'LWP::UserAgent';
 requires 'Mojo::Template';
 requires 'Parallel::ForkManager';
+requires 'TAP::Parser';
+requires 'Term::ANSIColor';
 requires 'Test::More', '0.88';
 requires 'Test::TCP';
 requires 'perl', '5.010';
