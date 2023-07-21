86478afb3c7a8c3684af116dbb9c0fdfb87f8b94
Kenichi Ishigaki <ishigaki@cpan.org>
2023-07-21 12:35:35 +0900

    Update Dockerfiles and docker-entrypoint.shs

--- centos6/docker-entrypoint.sh
+++ centos6/docker-entrypoint.sh
@@ -4,8 +4,6 @@ set -e
 service mysqld start
 service memcached start
 
-sed -i -E 's/inet_protocols = all/inet_protocols = ipv4/' /etc/postfix/main.cf
-
 mysql -e "create database if not exists mt_test character set utf8;"
 mysql -e "grant all privileges on mt_test.* to mt@localhost;"
 
