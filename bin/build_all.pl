417c91566ba82588d16596cce81c46ab4e4436a2
Kenichi Ishigaki <ishigaki@cpan.org>
2023-11-11 10:53:24 +0900

    Loosen the condition for successful built

--- bin/build_all.pl
+++ bin/build_all.pl
@@ -53,7 +53,7 @@ for my $name (@targets) {
     }
     system("docker build $name $tags" . ($no_cache ? " --no-cache" : "") . " 2>&1 | tee log/build_$name.log");
     my $log = path("log/build_$name.log")->slurp;
-    if ($log =~ m!(naming to docker.io/movabletype/test:$name done|Successfully built)!) {
+    if ($log =~ m!(naming to docker.io/movabletype/test:$name (\S+ )?done|Successfully built)!) {
         if ($log =~ /No package (.+) available/) {
             rename "log/build_$name.log" => "log/build_warn_$name.log";
         } else {
