c323104fbd1c8449c090e0f01c406772249e0a8f
Kenichi Ishigaki <ishigaki@cpan.org>
2025-05-20 03:04:58 +0900

    Add fedora42

--- bin/update_dockerfile.pl
+++ bin/update_dockerfile.pl
@@ -211,6 +211,43 @@ my %Conf = (
         nogpgcheck                     => 1,
         mysql_require_secure_transport => 1,
     },
+    fedora42 => {
+        from => 'fedora:42',
+        base => 'centos',
+        yum  => {
+            _replace => {
+                'mysql'        => '',
+                'mysql-server' => '',
+                'mysql-devel'  => '',
+                'procps'       => 'perl-Unix-Process',
+                'phpunit'      => '',
+            },
+            base   => [qw( distribution-gpg-keys glibc-langpack-en glibc-langpack-ja xz )],
+            images => [qw( libomp-devel )],
+        },
+        cpan => {
+            # https://github.com/DCIT/perl-CryptX/issues/118
+            no_test => [qw( CryptX App::Prove::Plugin::MySQLPool )],
+        },
+        remove_from_cpanfile => [qw( YAML::Syck )],
+        make_dummy_cert      => '/usr/bin',
+        make                 => {
+            # package is broken for unknown reason
+            GraphicsMagick => '1.3.43',
+        },
+        repo => {
+            mysql84 => [qw(mysql-community-server mysql-community-client mysql-community-libs-compat mysql-community-libs mysql-community-devel)],
+        },
+        mysql84 => {
+            # taken from https://dev.mysql.com/downloads/repo/yum/
+            rpm    => 'https://dev.mysql.com/get/mysql84-community-release-fc42-1.noarch.rpm',
+            enable => 'mysql-8.4-lts-community',
+            # enable => 'mysql-innovation-community',
+        },
+        patch     => ['Test-mysqld-1.0030', 'Crypt-DES-2.07'],
+        installer => 'dnf',
+        phpunit   => 11,
+    },
     fedora41 => {
         from => 'fedora:41',
         base => 'centos',
