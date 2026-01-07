6efc0dc4348baa009fd996ef90ad195643b95702
Kenichi Ishigaki <ishigaki@cpan.org>
2026-01-07 12:22:32 +0900

    Make sure images used in the workflows are placed and marked correctly

--- bin/test_readme.pl
+++ bin/test_readme.pl
@@ -3,9 +3,20 @@ use warnings;
 use Test::More;
 use Mojo::File qw/path/;
 use Data::Dump qw/dump/;
+use YAML;
 
 my %mapping;
+my $phase;
 for my $line (split /\n/, path('README.md')->slurp) {
+    if ($line =~ /^## .*for CI/) {
+        $phase = 'ci';
+    }
+    if ($line =~ /^## .*manual testing/) {
+        $phase = 'manual';
+    }
+    if ($line =~ /^## Special images/) {
+        $phase = 'special';
+    }
     next unless $line =~ /^\|/;
     next if $line     =~ /^\|(?:\-|image name)/;
     $line             =~ s/(^\|)|(\|$)//g;
@@ -18,10 +29,17 @@ for my $line (split /\n/, path('README.md')->slurp) {
         $mapping{$image} = { map { split / /, $_ } @extra };
     } else {
         my ($perl, $php, $mysql, $openssl) = @rest;
+        my $ci = $phase eq 'ci' ? 1 : $image =~ /\(\\2\)/ ? 2 : 0;
         $image =~ s/ .+$//;
         $mapping{$image} = { perl => $perl, php => $php, mysql => $mysql, openssl => $openssl };
+        $mapping{$image}{ci} = $ci if $ci;
     }
     $mapping{$image}{base} = $base;
+}
+
+my $used = {};
+if (-e './tmp/used_images.yml') {
+    $used = YAML::LoadFile('./tmp/used_images.yml');
 }
 
 for my $image (sort keys %mapping) {
@@ -41,11 +59,26 @@ for my $image (sort keys %mapping) {
             is $from => $mapping{$image}{$key} => "$image has correct $key";
             next;
         }
+        if ($key eq 'ci') {
+            if ($mapping{$image}{ci} == 1) {
+                ok $used->{$image}{develop}, "$image is used in workflow files";
+            }
+            if ($mapping{$image}{ci} == 2) {
+                ok $used->{$image} && !$used->{$image}{develop}, "$image is previously used in workflow files";
+            }
+            next;
+        }
         my $wanted = $key;
         $wanted = '(?:mysql|mariadb|postgresql)' if $key eq 'mysql';
         my ($version) = $log =~ /$wanted exists \((.+?)\)/i;
         is $version => $mapping{$image}{$key} => "$image has correct $key";
     }
+}
+
+for my $image (sort keys %$used) {
+    next if $mapping{$image}{ci};
+    next if $image =~ /addons8|chromiumdriver/;
+    fail "$image is no longer used in workflow files";
 }
 
 done_testing;
