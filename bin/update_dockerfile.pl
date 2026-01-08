d2a0eb76fe5cef23f3e3a50122d6d56eb4a911d3
Kenichi Ishigaki <ishigaki@cpan.org>
2026-01-08 11:47:00 +0900

    Add plucky and questing

--- bin/update_dockerfile.pl
+++ bin/update_dockerfile.pl
@@ -200,6 +200,51 @@ my %Conf = (
         patch   => ['Test-mysqld-1.0030'],
         phpunit => 11,
     },
+    plucky => {
+        from => 'ubuntu:plucky',
+        base => 'debian',
+        apt  => {
+            base => [qw( libx32stdc++-15-dev )],
+            php => [qw( php-mbstring php-xml )],
+        },
+        cpan => {
+            # cf. https://rt.cpan.org/Public/Bug/Display.html?id=156899
+            no_test  => [qw( GD XML::LibXML Web::Query )],
+            _replace => {
+                'Imager::File::AVIF' => '',
+            },
+        },
+        patch => ['Test-mysqld-1.0030', 'Crypt-DES-2.07', 'YAML-Syck-1.36'],
+        phpunit => 11,
+    },
+    questing => {
+        from => 'ubuntu:questing',
+        base => 'debian',
+        apt  => {
+            base => [qw( libx32stdc++-15-dev xz-utils )],
+            php => [qw( php-mbstring php-xml )],
+            _replace => {
+                'graphicsmagick' => '',
+                'imagemagick' => '',
+                'perlmagick' => '',
+                'libgraphics-magick-perl',
+            },
+        },
+        cpan => {
+            # cf. https://rt.cpan.org/Public/Bug/Display.html?id=156899
+            no_test  => [qw( GD XML::LibXML Web::Query )],
+            _replace => {
+                'Imager::File::AVIF' => '',
+            },
+        },
+        patch => ['Test-mysqld-1.0030', 'Crypt-DES-2.07', 'YAML-Syck-1.36'],
+        plenv => '5.42.0',
+        make => {
+            ImageMagick => '7.1.2-12',
+            GraphicsMagick => '1.3.46',
+        },
+        phpunit => 11,
+    },
     rawhide => {
         from => 'fedora:rawhide',
         base => 'centos',
