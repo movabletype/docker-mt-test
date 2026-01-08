a802926cde1cd74cb6dd61cf98f64ecc4e6bed72
Kenichi Ishigaki <ishigaki@cpan.org>
2026-01-08 14:43:59 +0900

    Use uname for better multi platform support

--- bin/update_dockerfile.pl
+++ bin/update_dockerfile.pl
@@ -451,7 +451,7 @@ my %Conf = (
             GraphicsMagick => '1.3.43',
         },
         remi => {
-            rpm    => 'https://ftp.riken.jp/Linux/remi/fedora/40/remi/x86_64/remi-release-40-1.fc40.remi.noarch.rpm',
+            rpm    => 'https://ftp.riken.jp/Linux/remi/fedora/40/remi/`uname -m`/remi-release-40-1.fc40.remi.noarch.rpm',
             module => {
                 reset  => 'php',
                 enable => 'php:remi-8.2',
